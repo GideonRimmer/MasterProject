e5ff53100f2543b04aa6f5ecd47aef1023cdf02f
Gideon Rimmer <gideon@portaplay.net>
2021-02-07 21:59:40 +0100

    New test scene for slapping minigame. Added slapping minigame.

---
+++ Assets/Scripts/RegisterSlap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterSlap : MonoBehaviour
+{
+    public GameObject slappedCharacter;
+
+    // slapRotationY should be different of the slap comes from the left or right.
+    // Default 230 for the east collider, 130 for the west collider.
+    public int slapRotationY;
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Mouse enter " + this.name);
+        slappedCharacter.GetComponent<SlapManager>().GetSlapped(slapRotationY);
+    }
+}
