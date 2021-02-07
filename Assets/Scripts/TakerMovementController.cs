57b81d5342d78d29e5010aee60935b72e5d81c71
Gideon Rimmer <gideon@portaplay.net>
2021-02-07 22:01:25 +0100

    Added Charisma stat. Still not in use.

--- Assets/Scripts/TakerMovementController.cs
+++ Assets/Scripts/TakerMovementController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TakerMovementController : MonoBehaviour
 {
+    public int startingCharisma = 7;
+    [SerializeField] private int currentCharisma;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentCharisma = startingCharisma;
     }
 
     // Update is called once per frame
