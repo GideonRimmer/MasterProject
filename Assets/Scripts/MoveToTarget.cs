a140aa4035d49e33b21bc24cbcca87e550d681dc
Gideon Rimmer <gideon@portaplay.net>
2021-02-09 11:21:11 +0100

    Followers follow entity by sphere of influence + charisma.

---
+++ Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour
+{
+    public Transform currentTarget;
+    public float moveSpeed;
+    public float minDistanceToTarget;
+    public float maxDistanceToTarget;
+    public float currentDistanceToTarget;
+
+    public bool isAttacking;
+
+    void Start()
+    {
+        isAttacking = false;
+    }
+
+    void Update()
+    {
+        if (currentTarget != null)
+        {
+            FollowTarget();
+        }
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        if (currentTarget == null || newTarget.GetComponentInParent<SphereOfInfluence>().currentCharisma > currentTarget.GetComponentInParent<SphereOfInfluence>().currentCharisma)
+        {
+            currentTarget = newTarget;
+        }
+    }
+
+    private void FollowTarget()
+    {
+        if (Vector3.Distance(transform.position, currentTarget.transform.position) < maxDistanceToTarget && Vector3.Distance(transform.position, currentTarget.transform.position) > minDistanceToTarget)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/SphereOfInfluence.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereOfInfluence : MonoBehaviour
+{
+    public int startingCharisma = 10;
+    public int currentCharisma;
+
+    public float sphereInitialRadius = 10f;
+    public float sphereCurrentRadius;
+
+    Collider[] followersInSphere;
+
+    private void Start()
+    {
+        currentCharisma = startingCharisma;
+        sphereCurrentRadius = sphereInitialRadius;
+    }
+
+    private void FixedUpdate()
+    {
+        // Detect when a collider enters the sphere.
+        // Note that this happens in every frame for evert collider (including terrain!).
+        followersInSphere = Physics.OverlapSphere(this.transform.position, sphereCurrentRadius);
+
+        foreach(var follower in followersInSphere)
+        {
+            if (follower.tag == "Follower")
+            {
+                //Debug.Log(follower.name + " entered sphere.");
+                follower.GetComponentInParent<MoveToTarget>().SetTarget(this.transform);
+            }
+        }
+    }
+
+    // Show sphere radius in scene view.
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, sphereCurrentRadius);
+    }
+}
