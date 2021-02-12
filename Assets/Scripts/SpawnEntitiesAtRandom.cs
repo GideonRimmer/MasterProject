911009911f0c5b7b122256b35558ad1a45ec342e
Gideon Rimmer <gideon@portaplay.net>
2021-02-12 17:57:36 +0100

    Added script with parameters for random level generation.

---
+++ Assets/Scripts/SpawnEntitiesAtRandom.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEntitiesAtRandom : MonoBehaviour
+{
+    public GameObject followerPrefab;
+    public GameObject takerPrefab;
+
+    public int minX;
+    public int maxX;
+    public int minZ;
+    public int maxZ;
+    private int xPosition;
+    private int yPosition = 3;
+    private int zPosition;
+
+    public int followerCount;
+    public int numberOfFollowers;
+    private int takerCount;
+    public int numberOfTakers = 10;
+
+    void Start()
+    {
+        StartCoroutine(SpawnFollowers());
+        StartCoroutine(SpawnTakers());
+    }
+
+    IEnumerator SpawnFollowers()
+    {
+        while (followerCount < numberOfFollowers)
+        {
+            xPosition = Random.Range(minX, maxX);
+            zPosition = Random.Range(minZ, maxZ);
+            Instantiate(followerPrefab, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity);
+
+            yield return new WaitForSeconds(0.01f);
+            followerCount += 1;
+        }
+    }
+
+    IEnumerator SpawnTakers()
+    {
+        while (takerCount < numberOfTakers)
+        {
+            xPosition = Random.Range(minX, maxX);
+            zPosition = Random.Range(minZ, maxZ);
+            Instantiate(takerPrefab, new Vector3(xPosition, yPosition, zPosition), Quaternion.identity);
+
+            yield return new WaitForSeconds(0.01f);
+            takerCount += 1;
+        }
+    }
+}
