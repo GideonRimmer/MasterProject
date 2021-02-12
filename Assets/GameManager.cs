70e47f6bf5d9a7500bae84a30cb9da7269194181
Gideon Rimmer <gideon@portaplay.net>
2021-02-12 22:02:18 +0100

    Added restart button (R) and quit button (esc).

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+        // Restart by pressing R.
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(0);
+        }
+
+        // Quit the game by pressing ESC.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
