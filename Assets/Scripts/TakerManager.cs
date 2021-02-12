66e47644acc067b537e9272e7d12e8ea2c0d46de
Gideon Rimmer <gideon@portaplay.net>
2021-02-12 17:57:06 +0100

    Updated prefabs and related scripts.

---
+++ Assets/Scripts/TakerManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakerManager : MonoBehaviour
+{
+    public int minCharisma;
+    public int maxCharisma;
+    [SerializeField] private int randomCharisma;
+
+    void Awake()
+    {
+        randomCharisma = Random.Range(minCharisma, maxCharisma + 1);
+        this.GetComponent<SphereOfInfluence>().startingCharisma = randomCharisma;
+    }
+}
