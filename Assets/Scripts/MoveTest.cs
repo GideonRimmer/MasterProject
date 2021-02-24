dca1f06c20a35f44a5e8384307118fe69ddc29f6
Gideon Rimmer <gideon@portaplay.net>
2021-02-24 19:22:48 +0100

    Test script.

---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidbody.velocity = transform.forward * moveSpeed;
+            //rigidbody.velocity = transform.up * moveSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rigidbody.velocity = transform.right * moveSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rigidbody.velocity = transform.right * -moveSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rigidbody.velocity = transform.forward * -moveSpeed;
+            //rigidbody.velocity = transform.up * -moveSpeed;
+        }
+
+    }
+}
