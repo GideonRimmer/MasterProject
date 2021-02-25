449e4edd99618c5a3c2bf307ee0118f0d0bc2c87
Gideon Rimmer <gideon@portaplay.net>
2021-02-25 23:04:36 +0100

    Created filter scripts for dynamic boids.

---
+++ Assets/Boids Filter Scripts/DynamicPhysicsLayerFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter Dynamic/Physics Layer")]
+public class PhysicsLayerFilterDynamic : ContextFilterDynamic
+{
+    public LayerMask mask;
+
+    public override List<Transform> Filter(FlockAgentDynamic agent, List<Transform> original)
+    {
+        List<Transform> filtered = new List<Transform>();
+
+        // Check if a transform is on the filtered physics layer.
+        foreach (Transform item in original)
+        {
+            // If this is true, then the item we're checking is on the same layer as the mask is checking.
+            if (mask == (mask | (1 << item.gameObject.layer)))
+            {
+                filtered.Add(item);
+            }
+        }
+        return filtered;
+    }
+}
---
+++ Assets/Boids Filter Scripts/DynamicSameFlockFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter Dynamic/Same Flock")]
+public class DynamicSameFlockFilter : ContextFilterDynamic
+{
+    public override List<Transform> Filter(FlockAgentDynamic agent, List<Transform> original)
+    {
+        List<Transform> filtered = new List<Transform>();
+
+        // Iterate through the original list, and determine if a new transform has the FlockAgent component AND is of the same flock.
+        // If both are true, add to the list of filtered transforms.
+        foreach (Transform item in original)
+        {
+            FlockAgentDynamic itemAgent = item.GetComponent<FlockAgentDynamic>();
+            if (itemAgent != null && itemAgent.AgentFlock == agent.AgentFlock)
+            {
+                filtered.Add(item);
+            }
+        }
+        return filtered;
+    }
+}
