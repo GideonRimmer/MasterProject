d2453a4705652ff4a5e3d143043a50a0dfcd3f57
Gideon Rimmer <55274618+GideonRimmer@users.noreply.github.com>
2021-04-28 18:16:19 +0200

    Renamed levels and added NextLevel trigger.

---
+++ Assets/Scripts/LoadNextLevelOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevelOnTrigger : MonoBehaviour
+{
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameManager.LoadNextLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
