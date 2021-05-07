632094bb89c723566637f7c7c455900e59d21c0b
Gideon Rimmer <55274618+GideonRimmer@users.noreply.github.com>
2021-05-08 00:11:24 +0200

    Created script for triggering followers' option to become traitors.

---
+++ Assets/Scripts/TraitorTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraitorTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // When a Player Follower enters this trigger, turn on canBeTraitor.
+        if (other.CompareTag("Follower") && other.GetComponentInParent<FollowerManager>() != null && other.GetComponentInParent<FollowerManager>().currentLeader.CompareTag ("Player"))
+        {
+            Debug.Log(other.gameObject.name + " Can be traitor.");
+            other.GetComponentInParent<FollowerManager>().canBeTraitor = true;
+        }
+    }
+}
