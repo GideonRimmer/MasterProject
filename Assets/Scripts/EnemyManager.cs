04521d3beb8556a9692c3bdbc5db7db2fedfab2e
Gideon Rimmer <55274618+GideonRimmer@users.noreply.github.com>
2021-06-02 18:27:51 +0200

    Enemies return to starting point of too far from target.

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -136,6 +136,7 @@ public class EnemyManager : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(newDirection);
         */
 
+        currentDistanceToTarget = Vector3.Distance(transform.position, enemyTarget.position);
         destination = enemyTarget;
         navMeshAgent.SetDestination(destination.position);
 
