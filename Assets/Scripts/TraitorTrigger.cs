4aad26d84f14d4809537d1065ecaf33209ddf1a2
Gideon Rimmer <55274618+GideonRimmer@users.noreply.github.com>
2021-06-02 22:14:21 +0200

    Added null check to TraitorTrigger.

--- Assets/Scripts/TraitorTrigger.cs
+++ Assets/Scripts/TraitorTrigger.cs
@@ -7,7 +7,7 @@ public class TraitorTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // When a Player Follower enters this trigger, turn on canBeTraitor.
-        if (other.CompareTag("Follower") && other.GetComponentInParent<FollowerManager>() != null && other.GetComponentInParent<FollowerManager>().currentLeader.CompareTag ("Player"))
+        if (other.CompareTag("Follower") && other.GetComponentInParent<FollowerManager>() != null && other.GetComponentInParent<FollowerManager>().currentLeader != null && other.GetComponentInParent<FollowerManager>().currentLeader.CompareTag ("Player"))
         {
             Debug.Log(other.gameObject.name + " Can be traitor.");
             other.GetComponentInParent<FollowerManager>().canBeTraitor = true;
