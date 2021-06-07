5d619451a7b38758b3c35b28fce5e769d79af7ac
Gideon Rimmer <55274618+GideonRimmer@users.noreply.github.com>
2021-06-07 17:29:21 +0200

    Updated object prefabs, and random starting Y rotation for trees.

---
+++ Assets/Scripts/RandomStartRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStartRotation : MonoBehaviour
+{
+    public bool applyRandomRotation;
+    [SerializeField] private float angleY;
+
+    void Awake()
+    {
+        if (applyRandomRotation == true)
+        {
+            angleY = Random.Range(0, 360);
+            transform.eulerAngles = new Vector3(transform.rotation.x, angleY, transform.rotation.z);
+        }
+    }
+}
