801cbe162ac3218de1561ab51aed25723408f26e
Gideon Rimmer <55274618+GideonRimmer@users.noreply.github.com>
2021-06-08 16:52:12 +0200

    Play random sound script.

---
+++ Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    // See video for reference:
+    // https://www.youtube.com/watch?v=Bnm8mzxnwP8&ab_channel=JasonWeimannJasonWeimann
+
+    [SerializeField] private AudioClip[] clips;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandomClip()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[Random.Range(0, clips.Length)];
+    }
+}
